1fde94c2f39d8511ac87262f9aea69a872cae7ad
gytislaz <gytislaz@gmail.com>
2019-03-06 14:55:08 +0200

    add basic category dropdown

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import { map } from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  private apiUrl = 'https://good-works-summer-backend.herokuapp.com/initialdata/categories?fbcl' +
+    'id=IwAR0hQ7jbFgoTIp4A9DVA_sssOcn2Ng0vOMUTTB0ZatH01xxSUxs8RWZcl7A'
+  data: any = {};
+  category: string;
 
-  constructor() { }
+  constructor(private http: Http) {
+    this.getCategories();
+    this.getDataaa();
+  }
+
+  getDataaa() {
+    return this.http.get(this.apiUrl)
+      .pipe(map((res: Response) => res.json()));
+  }
+
+  getCategories() {
+    this.getDataaa().subscribe(data => {
+      console.log(data);
+      this.data = data
+    })
+  }
 
   ngOnInit() {
   }
+
 
 }
