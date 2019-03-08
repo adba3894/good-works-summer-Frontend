5bdeee1a24095335db38a555b199daa8a5db5f0c
Deividas <deividas.skama@hotmail.com>
2019-03-08 15:21:44 +0200

    update registration form

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -2,7 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {map} from 'rxjs/operators';
 import {Router} from '@angular/router';
+<<<<<<< HEAD
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+=======
+import {FormControl, FormGroup} from '@angular/forms';
+>>>>>>> update registration form
 
 @Component({
   selector: 'app-registration',
@@ -12,8 +16,15 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 export class RegistrationComponent implements OnInit {
   private categoryApiUrl = 'https://good-works-summer-backend.herokuapp.com/initialdata/categories';
   private citiesApiUrl = 'https://good-works-summer-backend.herokuapp.com/initialdata/cities';
+<<<<<<< HEAD
   categories = [];
   cities = [];
+=======
+  category = [];
+  cities = [];
+
+  teamForm: FormGroup;
+>>>>>>> update registration form
 
   registerForm: FormGroup;
   submitted = false;
@@ -22,6 +33,7 @@ export class RegistrationComponent implements OnInit {
 
   }
 
+<<<<<<< HEAD
   ngOnInit() {
     this.getCities().subscribe(data => {
       this.cities = data;
@@ -40,29 +52,60 @@ export class RegistrationComponent implements OnInit {
     });
   }
 
+=======
+>>>>>>> update registration form
   getData(ApiURL) {
     return this.http.get(ApiURL)
       .pipe(map((res: Response) => res.json()));
   }
 
   getCities() {
+<<<<<<< HEAD
     return this.getData(this.citiesApiUrl);
+=======
+    this.getData(this.citiesApiUrl).subscribe(data => {
+      this.cities = data
+    })
+>>>>>>> update registration form
   }
 
   getCategories() {
     return this.getData(this.categoryApiUrl);
   }
 
+<<<<<<< HEAD
   get registerFormControls() {
     return this.registerForm.controls;
   }
 
+=======
+  ngOnInit() {
+    this.getCities();
+    this.getCategories();
+    this.teamForm = new FormGroup({
+      teamLeadName: new FormControl(),
+      teamLeadEmail: new FormControl(),
+      teamName: new FormControl(),
+      city: new FormControl(),
+      organization: new FormControl(),
+      ideaForJob: new FormControl(),
+      category: new FormControl(),
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.teamForm.value);
+    this.goToSuccess();
+  }
+
+>>>>>>> update registration form
   goToHome() {
     this.router.navigateByUrl('');
   }
 
   goToSuccess() {
     this.router.navigateByUrl('registration/success');
+    console.log(this.teamForm)
   }
 
   onSubmit() {
