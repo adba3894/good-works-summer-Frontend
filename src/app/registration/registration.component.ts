c293948c2a74f64f6839c26e35c7ca7aca55d057
gytislaz <gytislaz@gmail.com>
2019-03-11 15:39:08 +0200

    add repository admin-login

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit, Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
-import {map} from 'rxjs/operators';
-import {Router} from '@angular/router';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -39,7 +39,7 @@ export class RegistrationComponent implements OnInit {
       city: ['', Validators.required],
       organization: ['', Validators.required],
       ideaForJob: ['', Validators.required],
-      category: ['', Validators.required],
+      category: ['', Validators.required]
     });
   }
 
@@ -70,23 +70,23 @@ export class RegistrationComponent implements OnInit {
 
   submitForPost(teamForm: FormGroup) {
     if (teamForm.valid) {
-      this.jsonForm = this.formBuilder.group({	
+      this.jsonForm = this.formBuilder.group({
         'leadName': teamForm.get('teamLeadName').value,
         'leadEmail': teamForm.get('teamLeadEmail').value,
         'teamName': teamForm.get('teamName').value,
-        'city': { 
-          'id': this.cities.find(city => city.name == teamForm.get('city').value).id,
+        'city': {
+          'id': this.cities.find(city => city.name === teamForm.get('city').value).id,
           'name': teamForm.get('city').value
         },
         'ideas': [[{
           'description': teamForm.get('ideaForJob').value,
           'project': {
-            'category': teamForm.get('category').value.toUpperCase().replace(/ /g,'_')
+            'category': teamForm.get('category').value.toUpperCase().replace(/ /g, '_')
           }
         }]],
         'organization': teamForm.get('organization').value
       });
-      let rawJsonFormValue = this.jsonForm.getRawValue();
+      const rawJsonFormValue = this.jsonForm.getRawValue();
       this.http.post(this.rootApiUrl + '/registration', rawJsonFormValue).subscribe(res => {
       });
     }
@@ -97,7 +97,6 @@ export class RegistrationComponent implements OnInit {
     if (this.teamForm.invalid) {
       return;
     }
-    alert('SUCCESS!!');
     this.goToSuccess();
     this.submitForPost(this.teamForm);
   }
