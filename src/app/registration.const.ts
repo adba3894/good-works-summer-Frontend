4f3c3c44d79333440290819c1379f89f46bb18cf
gytislaz <gytislaz@gmail.com>
2019-03-14 20:06:52 +0200

    fix url constant file and dependencies

---
+++ src/app/registration.const.ts
@@ -0,0 +1,3 @@
+export const ROOT_API_URL = 'https://good-works-summer-backend.herokuapp.com';
+export const CITIES_API_URL = 'https://good-works-summer-backend.herokuapp.com/initialdata/cities';
+export const CATEGORY_API_URL = 'https://good-works-summer-backend.herokuapp.com/initialdata/categories';
--- src/app/services/job-registration-service/constants.ts
+++
@@ -1,5 +0,0 @@
-export class Constants {
-  static categoryApiUrl = 'https://good-works-summer-backend.herokuapp.com/initialdata/categories';
-  static citiesApiUrl = 'https://good-works-summer-backend.herokuapp.com/initialdata/cities';
-  static rootApiUrl = 'https://good-works-summer-backend.herokuapp.com';
-}
