cdaaacc5859762b572a654591a2627bf2b7189a0
alyvos_alyvos <arina.cesnulyte@gmail.com>
2019-03-16 19:03:12 +0200

    project tab functionality

--- src/app/project-page/project-page.component.ts
+++ src/app/project-page/project-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
-import { ProjectPageService} from '../services/project-page-service/project-page.service';
-import { CATEGORY_API_URL } from '../registration.const';
+import { ProjectPageService } from '../services/project-page-service/project-page.service';
 import { TEAMS_API_URL } from '../registration.const';
+import { FILTERED_TEAMS_API_URL } from '../registration.const';
+
 
 @Component({
   selector: 'app-project-page',
@@ -20,11 +21,43 @@ export class ProjectPageComponent implements OnInit {
   ngOnInit() {
     this.projectPageService.getTeamsData(TEAMS_API_URL)
       .subscribe(data => {
-      this.teams = data;
-    });
-    this.projectPageService.getCategoriesData(CATEGORY_API_URL)
-      .subscribe(data => {
-        this.categories = data;
+        this.teams = data;
       });
+  }
+
+  get_beautiful_environment_data() {
+    this.projectPageService.getTeamsData(FILTERED_TEAMS_API_URL + '/BEAUTIFUL_ENVIRONMENT')
+      .subscribe(data => {
+        this.teams = data;
+      });
+  }
+
+  get_giving_back_data() {
+    this.projectPageService.getTeamsData(FILTERED_TEAMS_API_URL + '/GIVING_BACK_TO_SOCIETY')
+      .subscribe(data => {
+        this.teams = data;
+      });
+  }
+
+  get_helping_animals_data() {
+    this.projectPageService.getTeamsData(FILTERED_TEAMS_API_URL + '/HELPING_ANIMALS')
+      .subscribe(data => {
+        this.teams = data;
+      });
+  }
+
+  get_sharing_knowledge_data() {
+    this.projectPageService.getTeamsData(FILTERED_TEAMS_API_URL + '/SHARING_KNOWLEDGE')
+      .subscribe(data => {
+        this.teams = data;
+      });
+  }
+
+  goToIdeas() {
+    this.router.navigateByUrl('/ideas');
+  }
+
+  goToRegistration() {
+    this.router.navigateByUrl('/registration');
   }
 }
