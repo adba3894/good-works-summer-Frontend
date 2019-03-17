a85e9a0bc59d23594472f26f24cd59edbb6013ce
Arvydas Baranauskas <arvydas1998@gmail.com>
2019-03-18 00:24:53 +0200

    implementing admin functionality in progress, trying to solve error

--- src/app/services/admin-login-service/admin-login.service.ts
+++ src/app/services/admin-login-service/admin-login.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ADMIN_LOGIN_API_URL } from '../../registration.const';
 import { Cookie } from 'ng2-cookies';
-import { Headers } from '@angular/http';
+import { Headers, Http, RequestOptions } from '@angular/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminLoginService {
   jsonForm: FormGroup;
-  constructor(private adminLoginServiceHttp: HttpClient, private adminLoginServiceFormBuilder: FormBuilder) {
+  constructor(private adminLoginServiceHttp: Http, private adminLoginServiceFormBuilder: FormBuilder, private router: Router) {
   }
 
   postAdminCredentials(adminForm: FormGroup): Observable<any> {
@@ -20,7 +20,18 @@ export class AdminLoginService {
       'password': adminForm.get('password').value,
     });
     const rawJsonFormValue = this.jsonForm.getRawValue();
-    return this.adminLoginServiceHttp.post<any>(ADMIN_LOGIN_API_URL, rawJsonFormValue, { headers : this.getHeaders() })
+    const headers = new Headers({'Content-type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + btoa('marcus-client:marcus-secret')});
+    const options = new RequestOptions({ headers: headers });
+    return this.adminLoginServiceHttp.post(ADMIN_LOGIN_API_URL, rawJsonFormValue, options)
+      .map(res => res.json())
+      .subscribe(
+        data => this.saveToken(data)
+      );
+
+    // console.log(this.jsonForm);
+    // console.log(this.getHeaders());
+    // return this.adminLoginServiceHttp.post(ADMIN_LOGIN_API_URL, rawJsonFormValue, { headers : this.getHeaders() })
       // {headers: {headers: this.getHeaders()}new HttpHeaders()
       //     .set('Authorization',
       //       'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.e' +
@@ -34,6 +45,26 @@ export class AdminLoginService {
     const headers = new Headers({'Content-type': 'application/json',
       'Authorization': 'Bearer ' + Cookie.get('access_token')});
     return headers;
+  }
+
+  // obtainAccessToken(username: string, password: string){
+  //   const headers = new Headers({'Content-type': 'application/x-www-form-urlencoded',
+  //     'Authorization': 'Basic ' + btoa('marcus-client:marcus-secret')});
+  //   const options = new RequestOptions({ headers: headers });
+  //   this.adminLoginServiceHttp.post(ADMIN_LOGIN_API_URL, rawJsonFormValue, options)
+  //     .map(res => res.json())
+  //     .subscribe(
+  //       data => this.saveToken(data),
+  //       err => alert('Invalid Credentials')
+  //     );
+  // }
+
+
+  saveToken(token) {
+    const expireDate = new Date().getTime() + (1000 * token.expires_in);
+    Cookie.set('access_token', token.access_token, expireDate);
+    console.log('Obtained Access token');
+    this.router.navigate(['/admin/project']);
   }
 
   // butinai padarykit -> npm install ng2-cookies
