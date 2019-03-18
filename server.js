72eb3a3b918f65cc29fcaf16938aa823ef1bb002
Arvydas Baranauskas <arvydas1998@gmail.com>
2019-03-18 20:15:27 +0200

    bug fix

--- server.js
+++ server.js
@@ -7,10 +7,3 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
-
-app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
