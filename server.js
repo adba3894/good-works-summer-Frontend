a446c0a0fac2f08f9a38575474291bf344c2b9a5
Arvydas Baranauskas <arvydas1998@gmail.com>
2019-03-18 19:34:10 +0200

    bug fix

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 
 // Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.use(express.static('dist'));
+app.get = function(s, f) {
+
+};
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
