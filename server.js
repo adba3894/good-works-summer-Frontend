f4675da184f55fbd1ae3ddcfaa3314b0b49d2e06
Arvydas Baranauskas <arvydas1998@gmail.com>
2019-03-18 20:01:21 +0200

    bug fix

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
+app.get = function(s, f) {
+  
+};
 app.get('*', (request, response) => {
   response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
