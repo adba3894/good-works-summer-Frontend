a460df7faa60f8a42a71d92275b32243163a41d4
Arvydas Baranauskas <arvydas1998@gmail.com>
2019-03-20 16:12:26 +0200

    heroku fix try

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 
 var express = require('express');
 var compression = require('compression');
-var PORT = process.env.PORT || 8080;
+var proxy = require('http-proxy-middleware');
+var API_HOST = process.env.API_HOST || 'localhost:8080';
+var PORT = process.env.PORT || 8080
 
-var buildPath = 'dist';
+var buildPath = 'dist'
 
 // Initialize
 var app = express();
@@ -13,6 +15,15 @@ app.use(express.static(buildPath));
 
 // Enable gzip response compression
 app.use(compression());
+
+// Enable proxy to api
+app.use('/api', proxy({
+  target: API_HOST,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': ''
+  }
+}));
 
 // Otherwise serve index.html
 app.get('*', function (req, res) {
